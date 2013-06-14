f79da5aa8d7a71cb9000268d20167e32963f4b74
atupal@foxmail.com <atupal@foxmail.com@ececca69-a9b5-4a8b-8039-f91a18f81fae>
2013-06-14 14:19:30 +0000

    form
    git-svn-id: https://svn.sinaapp.com/3gqq67/1@4 ececca69-a9b5-4a8b-8039-f91a18f81fae

--- application/views/index.py
+++ application/views/index.py
@@ -6,8 +6,8 @@ def index():
     return '''
     <html>
         <form action="/dianzan">
-            <input type="text" value="qq" />
-            <input type="text" value="pwd" />
+            <input type="text" value="qq" name="qq"/>
+            <input type="text" value="pwd" name="pwd" />
             <input type="submit" value="confirem">
         </form>
     </html>
