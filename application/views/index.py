2515910bcb65743a038b11910ff3cfec93afd8ee
atupal@foxmail.com <atupal@foxmail.com@ececca69-a9b5-4a8b-8039-f91a18f81fae>
2013-06-14 21:37:55 +0000

    test
    git-svn-id: https://svn.sinaapp.com/3gqq67/1@45 ececca69-a9b5-4a8b-8039-f91a18f81fae

--- application/views/index.py
+++ application/views/index.py
@@ -14,67 +14,18 @@ def index():
             <input type="submit" value="confirem">
         </form-->
 
-        <!--div class="rain unfocus start">
+        <div class="rain unfocus start">
             <div class="border unfocus start">
                 <form id="login" action="/dianzan" method="post">
                     <label for="qq">qq</label>
                     <input name="qq" type="text" placeholder="qq">
                     <label for="pass">Password</label>
                     <input name="pwd" type="password" placeholder="Password">
+                    <input type="submit" value="confirm">
                 </form>
             </div>
-            <input type="submit" value="confirm" onclick='document.getElementById("login").submit()'>
-        </div-->
-        <div id="container">
-           <div class="description">
-                 Recommended browsers: Safari, Chrome<br>
-                       Alternate animation: Firefox 4<br>
-                           </div>
-
-                               <!-- Glowform -->
-                                       <div class="rain unfocus start">
-                                                <div class="border unfocus start">
-                                                                <form>
-                                                                                    <label for="email">Email</label>
-                                                                                                        <input name="email" type="text" placeholder="Email">
-                                                                                                                            <label for="pass">Password</label>
-                                                                                                                                                <input name="pass" type="password" placeholder="Password">
-                                                                                                                                                                </form>
-                                                                                                                                                                            </div>
-                                                                                                                                                                                        <input type="submit" value="LOG IN">
-                                                                                                                                                                                                </div>
-                                                                                                                                                                                                    <!-- /Glowform -->
-
-                                                                                                                                                                                                        <h1><a href="http://github.com/kaylarose/Glowform">Glowform</a>
-                                                                                                                                                                                                              <span class="small">by <a href="http://github.com/kaylarose">kaylarose</a></span></h1>
-
-                                                                                                                                                                                                                  <div class="description">
-                                                                                                                                                                                                                       CSS3 (image-less) Glowing Login Form (inspired by Dragon Labs).<br><br>
-                                                                                                                                                                                                                             Webkit browsers recommended. Use Firefox 4 to see the fallback animation.
-                                                                                                                                                                                                                                 </div>
-                                                                                                                                                                                                                                     <p>
-                                                                                                                                                                                                                                          * Disclaimer: This is meant as a CSS3 tech demo, not a best practices tutorial.
-                                                                                                                                                                                                                                              </p>
-                                                                                                                                                                                                                                                  <h2>Author(s)</h2>
-                                                                                                                                                                                                                                                      <p>Kayla Martin (kayla.rose.martin@gmail.com &amp; http://gemdash.com)</p>
-
-
-                                                                                                                                                                                                                                                          <h2>Download</h2>
-                                                                                                                                                                                                                                                              <p>
-                                                                                                                                                                                                                                                                    You can download this project in either
-                                                                                                                                                                                                                                                                          <a href="http://github.com/kaylarose/Glowform/zipball/master">zip</a> or
-                                                                                                                                                                                                                                                                                <a href="http://github.com/kaylarose/Glowform/tarball/master">tar</a> formats.
-                                                                                                                                                                                                                                                                                    </p>
-                                                                                                                                                                                                                                                                                        <p>You can also clone the project with <a href="http://git-scm.com">Git</a>
-                                                                                                                                                                                                                                                                                              by running:
-                                                                                                                                                                                                                                                                                                        </p><pre>$ git clone git://github.com/kaylarose/Glowform</pre>
-                                                                                                                                                                                                                                                                                                            <p></p>
-
-                                                                                                                                                                                                                                                                                                                <div class="footer">
-                                                                                                                                                                                                                                                                                                                      get the source code on GitHub : <a href="http://github.com/kaylarose/Glowform">kaylarose/Glowform</a>
-                                                                                                                                                                                                                                                                                                                          </div>
-
-                                                                                                                                                                                                                                                                                                                            </div>
+            <!--input type="submit" value="confirm" onclick='document.getElementById("login").submit()'-->
+        </div>
     </html>
     '''
 
