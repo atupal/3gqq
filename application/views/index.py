d9358b43290eb70b13b03f95cfd1695e3f86f736
atupal@foxmail.com <atupal@foxmail.com@ececca69-a9b5-4a8b-8039-f91a18f81fae>
2013-06-14 14:20:23 +0000

    form
    git-svn-id: https://svn.sinaapp.com/3gqq67/1@6 ececca69-a9b5-4a8b-8039-f91a18f81fae

--- application/views/index.py
+++ application/views/index.py
@@ -5,7 +5,7 @@ from application import app
 def index():
     return '''
     <html>
-        <form action="/dianzan" method="get">
+        <form action="/dianzan" method="post">
             <input type="text" value="qq" name="qq"/>
             <input type="text" value="pwd" name="pwd" />
             <input type="submit" value="confirem">
