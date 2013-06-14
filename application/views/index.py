0d2faf3a180859ec8fb54fe27715dafbef989d5d
atupal@foxmail.com <atupal@foxmail.com@ececca69-a9b5-4a8b-8039-f91a18f81fae>
2013-06-14 14:18:33 +0000

    form
    git-svn-id: https://svn.sinaapp.com/3gqq67/1@3 ececca69-a9b5-4a8b-8039-f91a18f81fae

--- application/views/index.py
+++ application/views/index.py
@@ -3,5 +3,13 @@ from application import app
 
 @app.route('/')
 def index():
-    return 't'
+    return '''
+    <html>
+        <form action="/dianzan">
+            <input type="text" value="qq" />
+            <input type="text" value="pwd" />
+            <input type="submit" value="confirem">
+        </form>
+    </html>
+    '''
 
