3bf34e1fb1905937ab980b6d7f67ec24042773f7
atupal@foxmail.com <atupal@foxmail.com@ececca69-a9b5-4a8b-8039-f91a18f81fae>
2013-06-14 21:39:45 +0000

    test
    git-svn-id: https://svn.sinaapp.com/3gqq67/1@49 ececca69-a9b5-4a8b-8039-f91a18f81fae

--- application/views/index.py
+++ application/views/index.py
@@ -16,10 +16,8 @@ def index():
         <form id="login" action="/dianzan" method="post">
             <label for="qq">qq</label>
             <input name="qq" type="text" placeholder="qq">
-            <br/>
             <label for="pass">Password</label>
             <input name="pwd" type="password" placeholder="Password">
-            <br/>
             <input type="submit" value="confirm">
         </form>
     </html>
