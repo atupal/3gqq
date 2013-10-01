8ad350b704978e1c9c0da1358aff9eea9550eb4e
atupal@foxmail.com <atupal@foxmail.com@ececca69-a9b5-4a8b-8039-f91a18f81fae>
2013-10-01 19:00:05 +0000

    typo
    git-svn-id: https://svn.sinaapp.com/3gqq67/1@135 ececca69-a9b5-4a8b-8039-f91a18f81fae

--- application/control/kvdbwrap.py
+++ application/control/kvdbwrap.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+
+import os
+RUN_LEVEL = 0
+if '/home/atupal' in os.environ.get('HOME', ''):
+  RUN_LEVEL = 1
+
 class KVDB(object):
   '''
     a simple wrapper of kvdb
@@ -21,9 +27,6 @@ class KVDB(object):
 
   def set(self, key, val):
     #print 'set:', key, val
-    dataVersion = json.loads(self.get('dataVersion'))
-    dataVersion['dataVersion'] = int(time.time() * 1000)
-    self.kv.set('dataVersion', json.dumps(dataVersion).encode('utf-8'))
     return self.kv.set(key.encode("utf-8"), val)
 
   def add(self, key, val):
@@ -31,23 +34,68 @@ class KVDB(object):
       同set， 但只在key不存在时起作用
     '''
     #print 'add:', key, val
-    dataVersion = json.loads(self.get('dataVersion'))
-    dataVersion['dataVersion'] = int(time.time() * 1000)
-    self.set('dataVersion', json.dumps(dataVersion))
-    if key.encode('utf-8').startswith('goodsId#'):  # 自动生成一个最小的商品ID
-      keys = self.getkeys_by_prefix(key)
-      for i in xrange(300):
-        if "%s%.3d" % (key.encode('utf-8'), i) not in keys:
-          if self.get(key):
-            return self.kv.set("%s%.3d" % (key.encode('utf-8'), i), val.encode('utf-8'))
-          return self.kv.add("%s%.3d" % (key.encode('utf-8'), i), val.encode('utf-8'))
-      return 'no enough id'
-    elif key.encode('utf-8').startswith('sortId#'):  # 自动生成一个最小的分类ID
-      keys = self.getkeys_by_prefix("sortId#")
-      if key.lstrip("sortId#") == "000":
-        begin = 0
-        end = 300
-      elif key.lstrip("sortId#") == "300":
-        begin = 300
-        end = 600
-      for i in xrange(begin, end):
+    if self.get(key):
+      return self.kv.set(key.encode("utf-8"), val.encode('utf-8'))
+    return self.kv.add(key.encode("utf-8"), val.encode('utf-8'))
+
+  def replace(self, key, val):
+    '''
+      同set， 但只在key存在时起作用
+    '''
+    #print 'replace:', key, val
+    return self.kv.replace(key.encode("utf-8"), val.encode('utf-8'))
+
+  def delete(self, key):
+    #print 'delete:', key
+    return self.kv.delete(key.encode('utf-8'))
+
+  def get(self, key):
+    #print "get:", key
+    return self.kv.get(key.encode('utf-8'))
+
+  def get_multi(self, keys, key_prefix=''):
+    '''
+      一次获取多个key的值，返回一个key/value的dict
+      keys: key的列表，类型必须为list
+      key_prefix: 所有key的前缀， 请求时会在所有key前加上该前缀， 返回值里所有的key都会去掉该前缀
+    '''
+    #print 'get_multi', keys
+    keys = [key.encode('utf-8') for key in keys]
+    return self.kv.get_multi(keys, key_prefix)
+
+  def get_by_prefix(self, prefix):
+    '''
+      返回一个list，item为一个(key, value)的tuple
+    '''
+    #print 'get_by_prefix:', prefix
+    return self.kv.get_by_prefix(prefix.encode('utf-8'), max_count=1000, start_key=None)
+  
+  def getkeys_by_prefix(self, prefix, max_count=1000, start_key=None):
+    #print 'getkeys_by_prefix:', prefix
+    return self.kv.getkeys_by_prefix(prefix.encode('utf-8'), max_count=max_count)
+
+  def get_info():
+    '''
+      返回一个kbdb信息的字典
+    '''
+    return self.kv.get_info()
+
+
+  ############################################################
+  #  my own's method
+  ############################################################
+  def update(self, key, item):
+    """更新某个item
+
+    item: 要更新的key/value map
+    """
+    items = self.get(key)
+    if items:
+      items = json.loads(items)
+      items.update(item)
+      self.set(key, json.dumps(items))
+      return json.dumps(items) 
+    return None
+
+  def fetch(self, handle=None, start=0, count=100):
+    return None
