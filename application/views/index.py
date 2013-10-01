ae71f6eff8db8e28d554a1167438c7730570276d
atupal@foxmail.com <atupal@foxmail.com@ececca69-a9b5-4a8b-8039-f91a18f81fae>
2013-10-01 08:17:33 +0000

    udpate
    git-svn-id: https://svn.sinaapp.com/3gqq67/1@119 ececca69-a9b5-4a8b-8039-f91a18f81fae

---
+++ application/views/index.py
@@ -0,0 +1,35 @@
+#-*- coding: utf-8 -*-
+
+from application import app
+from flask import request
+from flask import render_template
+
+from application.apps import dianzan
+import sys
+
+from application.apps.db_methods import add_task
+from application.apps.db_methods import init_db
+import logging
+import traceback
+from pprint import pprint as printf
+
+#from application import db
+
+@app.route('/')
+def index():
+  return render_template('layout.html')
+
+@app.route('/origin')
+def index_origin():
+    db = init_db()
+    cursor = db.cursor()
+    cursor.execute('''select * from feedback order by id DESC''')
+    ret = cursor.fetchall()
+    try:ret_list = [ [ _[1] , _[2], _[3] ] for _ in ret ]
+    except:ret_list = [ ['error', 'error', 'error'] ]
+
+    return render_template('index.html', comments = ret_list)
+
+@app.route('/dianzan_qq')
+def dianzan_qq():
+  return render_template('dianzan_qq.html')
